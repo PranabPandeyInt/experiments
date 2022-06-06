78f99f94a34a221983c2a5d8d73d103ec0ba5958
PranabPandeyInt <106058521+PranabPandeyInt@users.noreply.github.com>
2022-06-06 12:44:46 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import yaml
 from pathlib import Path
 path_directory = os.getcwd()
 file = Path(path_directory ).glob('*.yaml',recursive=True)
+print(file)
 for filename in file:
     yaml_file = open(filename)
     parsed_yaml = yaml.safe_load(yaml_file)
